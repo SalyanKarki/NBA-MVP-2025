9cea458e01f8931b0563dfa6dbbedf139e247129
Salyan Karki <salyankarki@gmail.com>
2025-02-27 13:36:36 -0600

    removed conversion script

---
+++ .ipynb_checkpoints/streamlitapp-checkpoint.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[13]:
+
+
+import streamlit as st
+import pandas as pd
+
+
+# In[16]:
+
+
+st.title("NBA MVP Top 5 Predictions (1982-2024) Using Ridge Regression")
+top_5_per_year = pd.read_csv("top_5_mvp_1982-2024.csv") 
+# Dropdown to select year
+selected_year = st.selectbox("Select a Year", sorted(df["Year"].unique(), reverse=True))
+
+# Filter data for the selected year
+filtered_df = top_5_per_year[top_5_per_year["Year"] == selected_year]
+
+# Display the table
+st.write(f"Top 5 MVP Candidates for {selected_year}:")
+st.dataframe(filtered_df[["Player", "Share", "Rk", "Predicted_Rk", "Diff"]].reset_index(drop=True))
+
+
+# In[17]:
+
+
+jupyter nbconvert --to script streamlitapp.ipynb
+
+
+# In[ ]:
+
+
+
+
